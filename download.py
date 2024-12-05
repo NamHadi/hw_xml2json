a0528ab28d5b32d2843c2a408627622ddbe9c46b
Naeemuddin Hadi <49169702+NamHadi@users.noreply.github.com>
2024-12-05 23:38:03 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,10 +1,10 @@
 import requests
 
-# Download the file
+# Download file
 url = "https://ufal.mff.cuni.cz/~zabokrtsky/courses/npfl125/data/dependency_trees_from_ud.tsv"  # Replace with actual URL
 response = requests.get(url)
 
-# Save the file
+# Save file
 with open("dependency_trees_from_ud.tsv", "wb") as f:
     f.write(response.content)
 
