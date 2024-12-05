5cac95561c1b84fd44a986d933008154b69fb32b
Naeemuddin Hadi <49169702+NamHadi@users.noreply.github.com>
2024-12-05 23:35:15 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 import requests
 
 # Download the file
-url = "https://example.com/dependency_trees_from_ud.tsv"  # Replace with actual URL
+url = "https://ufal.mff.cuni.cz/~zabokrtsky/courses/npfl125/data/dependency_trees_from_ud.tsv"  # Replace with actual URL
 response = requests.get(url)
 
 # Save the file
